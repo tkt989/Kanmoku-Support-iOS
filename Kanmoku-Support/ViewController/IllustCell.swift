c388351f2ad005c0817ca4503f133cbbc154b784
tkt989 <d.0508taka@gmail.com>
2018-04-15 01:08:58 +0900

    イラストを表示する機能を追加

---
+++ Kanmoku-Support/ViewController/IllustCell.swift
@@ -0,0 +1,13 @@
+// 
+//  IllustCell.swift
+//  Kanmoku-Support
+//
+//  Created by tkt989 on 2018/04/14.
+//  Copyright © 2018年 tkt989. All rights reserved.
+//
+
+import UIKit
+
+class IllustCell: UICollectionViewCell {
+    @IBOutlet public weak var illustView: UIImageView!
+}
