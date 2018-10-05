0f03f2a3614f8a75d8682ba5e0b2b4e0fa52a999
tkt989 <d.0508taka@gmail.com>
2018-10-06 01:31:03 +0900

    TTSクラスの修正

--- Kanmoku-Support/Model/TTS.swift
+++ Kanmoku-Support/Model/TTS.swift
@@ -8,14 +8,21 @@
 
 import AVFoundation
 
+/// 音声をスピーチするクラス
 public class TTS: NSObject, AVSpeechSynthesizerDelegate {
-    private var finishListners: [AVSpeechUtterance: () -> ()] = [:]
+    private var finishListeners: [AVSpeechUtterance: () -> ()] = [:]
     private var synth: AVSpeechSynthesizer
     
     override init() {
         self.synth = AVSpeechSynthesizer()
     }
     
+    /// 音声をスピーチする
+    ///
+    /// - Parameters:
+    ///   - text:
+    ///   - rate:
+    ///   - didFinish:
     func speech(_ text: String, rate: Float = 1.0, didFinish: @escaping () -> () = { () in return }) {
         let utterance = AVSpeechUtterance(string: text)
         utterance.rate = AVSpeechUtteranceDefaultSpeechRate * rate
@@ -23,11 +30,18 @@ public class TTS: NSObject, AVSpeechSynthesizerDelegate {
         
         self.synth.delegate = self
         self.synth.speak(utterance)
-        finishListners[utterance] = didFinish
+        finishListeners[utterance] = didFinish
     }
     
+    
+    
+    /// スピーチが終了したときのコールバック
+    ///
+    /// - Parameters:
+    ///   - synthesizer:
+    ///   - utterance:
     public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
-        finishListners[utterance]?()
-        finishListners.removeValue(forKey: utterance)
+        finishListeners[utterance]?()
+        finishListeners.removeValue(forKey: utterance)
     }
 }
