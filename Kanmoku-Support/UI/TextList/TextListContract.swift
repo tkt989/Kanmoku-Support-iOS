a4380613956ae2254b33bdb014329c14c680872e
tkt989 <d.0508taka@gmail.com>
2019-10-02 23:43:24 +0900

    #4 文章の並び替え機能を追加しました

---
+++ Kanmoku-Support/UI/TextList/TextListContract.swift
@@ -0,0 +1,23 @@
+// 
+//  TextListContract.swift
+//  Kanmoku-Support
+//
+//  Created by tkt989 on 2019/09/29.
+//  Copyright © 2019 tkt989. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol TextListViewProtocol {
+    func updateType(_ type: Type)
+    func updateOrder(_ order: Order)
+    func refreshTextList()
+    func back(_ text: String)
+}
+
+protocol TextListPresenterProtocol: UITableViewDataSource {
+    func onOrderClick()
+    func onTypeClick()
+    func onTextClick(_ index: Int)
+}
