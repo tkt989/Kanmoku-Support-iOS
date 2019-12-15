b4fe184b7a24548a1db87164b42eb3ad73deda00
tkt989 <tkt989.dev@gmail.com>
2019-12-15 21:27:55 +0900

    イラストの表示をフルスクリーンに変更、不要なコードを削除

--- Kanmoku-Support/UI/IllustSelect/IllustSelectViewController.swift
+++ Kanmoku-Support/UI/IllustSelect/IllustSelectViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class IllustSelectViewController: UICollectionViewController {
-    // MARK: Images
     let images = [
         "face1",
         "face2",
@@ -24,87 +23,37 @@ class IllustSelectViewController: UICollectionViewController {
         "face9"
     ]
     
-    // MARK: methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        // self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return images.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! IllustCell
-    
-        // Configure the cell
+
         let imageName = images[indexPath.row]
         cell.illustView.image = UIImage(named: imageName)
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    // Uncomment this method to specify if the specified item should be selected
+    
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         let cell = collectionView.cellForItem(at: indexPath) as! IllustCell
         let vc = IllustShowViewController()
         vc.illust = cell.illustView.image
+        vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true, completion: nil)
         return true
     }
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
--- Kanmoku-Support/ViewController/ViewController.swift
+++ Kanmoku-Support/ViewController/ViewController.swift
@@ -12,10 +12,6 @@ import NXDrawKit
 
 class ViewController: UIViewController {
     var image: UIImage = UIImage()
-    @IBOutlet weak var canvas: Canvas!
-    @IBOutlet weak var stack: UIStackView!
-    @IBOutlet weak var textButton: UIButton!
-    @IBOutlet weak var textButtonShadow: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     
@@ -30,59 +26,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func Show(_ sender: UIButton) {
-        performSegue(withIdentifier: "image", sender: nil)
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
-    }
-}
-// MARK: - CanvasDelegate
-extension ViewController: CanvasDelegate
-{
-    func brush() -> Brush? {
-        let brush = Brush()
-        brush.color = UIColor.black
-        brush.width = 10.0
-        return brush
-    }
-    
-    func canvas(_ canvas: Canvas, didUpdateDrawing drawing: Drawing, mergedImage image: UIImage?) {
-        self.image = image!
-    }
-    
-    func canvas(_ canvas: Canvas, didSaveDrawing drawing: Drawing, mergedImage image: UIImage?) {
-        // you can save merged image
-        //        if let pngImage = image?.asPNGImage() {
-        //            UIImageWriteToSavedPhotosAlbum(pngImage, self, #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
-        //        }
-        
-        // you can save strokeImage
-        //        if let pngImage = drawing.stroke?.asPNGImage() {
-        //            UIImageWriteToSavedPhotosAlbum(pngImage, self, #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
-        //        }
-        
-        //        self.updateToolBarButtonStatus(canvas)
-        
-        // you can share your image with UIActivityViewController
-        if let pngImage = image?.asPNGImage() {
-            let activityViewController = UIActivityViewController(activityItems: [pngImage], applicationActivities: nil)
-            activityViewController.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
-                if !completed {
-                    // User canceled
-                    return
-                }
-                
-                if activityType == UIActivity.ActivityType.saveToCameraRoll {
-                    let alert = UIAlertController(title: nil, message: "Image is saved successfully", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: .default))
-                    self.present(alert, animated: true)
-                }
-            }
-            activityViewController.popoverPresentationController?.sourceView = self.view
-            self.present(activityViewController, animated: true, completion: nil)
-        }
     }
 }
